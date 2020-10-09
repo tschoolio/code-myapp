e40bb5675382332498cf71852ad842450cdf97c0
Kestutis Maciulaitis <kestas@maciulaitis.lt>
2020-10-09 07:26:25 +0300

    Pacodeinkime pirma aplikacija

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(public alertController: AlertController) {}
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['Cancel', 'Open Modal', 'Delete']
+    });
+
+    await alert.present();
+  }
+  
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Neduosiu nieko.');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Take my money');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+}
